86b0e54e6bc8646566cd906b8ed68fb37e9063a2
unknown <Eze-jordan>
2025-07-29 16:58:07 +0100

    localhost:4200

--- src/main/java/com/esiitech/monbondocteurv2/config/SecurityConfig.java
+++ src/main/java/com/esiitech/monbondocteurv2/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:8080/swagger-ui/index.html")); // ← frontend autorisé
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // ← frontend autorisé
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
